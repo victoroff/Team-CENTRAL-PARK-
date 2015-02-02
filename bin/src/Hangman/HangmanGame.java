683ad7786a5b6a4c68024961a6cd97d346253bec
Nunev <teo_nunev@yahoo.com>
2015-02-03 00:07:32 +0200

    Updated the HangmanGame with try catch.

--- bin/src/Hangman/HangmanGame.java
+++ bin/src/Hangman/HangmanGame.java
@@ -1,5 +1,6 @@
 package Hangman;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class HangmanGame {
@@ -16,10 +17,16 @@ public class HangmanGame {
 			
 			while (m.isGameOver() == false && m.isWordGuessed() == false)  {
 			v.drowView(m.getNumberOffWrongAnswers(), m.getAvailableCharacters(), m.getMask());
-			char[] l = input.nextLine().toCharArray();
-			char curentLetter = l[0];
-			m.setChoice(curentLetter);
-			//System.out.println(m.getMask());
+			try {
+				char[] l = input.nextLine().toCharArray();
+				char curentLetter = l[0];
+				m.setChoice(curentLetter);
+				//System.out.println(m.getMask());
+			} catch (InputMismatchException e) {
+				System.err.println("No character was entered");
+			} catch (IndexOutOfBoundsException iob) {
+				System.err.println("No character was entered");
+			}
 			}
 			v.drowEnd(m.getNumberOffWrongAnswers(), wWord, m.isWordGuessed());
 			String choise = input.nextLine();
