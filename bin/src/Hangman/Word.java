29a3da8f0c37bf3d2a47bd1e221893544179dbe0
Nunev <teo_nunev@yahoo.com>
2015-02-02 23:59:29 +0200

    Commiting changes to the class

--- bin/src/Hangman/Word.java
+++ bin/src/Hangman/Word.java
@@ -9,8 +9,8 @@ public class Word {
 		String[] listOfWord = {"Sofia","Plovdiv","Varna","Veliko Tarnovo","Haskovo","Montana","Burgas","Gorna Orqhovica","Petric"};
 		int index = rnd.nextInt(listOfWord.length - 1);
 		String word = listOfWord[index];
-		return word;
+		return word;	
 	}
-	
-
+		
 }
+	
